b21fdf0e846e52bfc8b2c699b9e36e8496c764e0
Naren <narencodes@gmail.com>
2019-06-06 21:05:24 +0530

    Add lookup API to get the url string from hash

--- apiserver/api.go
+++ apiserver/api.go
@@ -96,6 +96,17 @@ func (api *ApiServer) lookup(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func (api *ApiServer) lookup(w http.ResponseWriter, r *http.Request) {
+	hash := mux.Vars(r)["hash"]
+	url, err := api.cache.HGet("urlmaps", hash).Result()
+	if err != nil {
+		fmt.Printf("Error : %v \n", err)
+		respondError(w, "Failure")
+		return
+	}
+	respond(w, "Success", url)
+}
+
 func NewServer() *ApiServer {
 	c, err := cache.NewCache("localhost", 6379)
 	if err != nil {
@@ -111,6 +122,7 @@ func (api *ApiServer) Start(host string, port int) error {
 	api.r.HandleFunc("/ping", ping).Methods("GET")
 	api.r.HandleFunc("/shortern", api.shotern).Methods("POST")
 	api.r.HandleFunc("/{hash}", api.lookup).Methods("GET")
+	api.r.HandleFunc("/lookup/{hash}", api.lookup).Methods("GET")
 	fmt.Println("Starting the server... ")
 	addr := fmt.Sprintf("%s:%d", host, port)
 	api.server = &http.Server{
